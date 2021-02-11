6319d66ed38356bab6c503ee387768b8a02d1a3d
igoradamenko <mail@igoradamenko.com>
2021-02-11 14:49:42 +0200

    Add linter

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-module.exports = function(options) {
+module.exports = options => {
   const aliases = Object.keys(options);
   const re = new RegExp(`^${aliases.map(x => escapeRegExp(x)).join('|')}$`);
 
   return {
     name: 'alias',
     setup(build) {
-      build.onResolve({ filter: re, namespace: 'file' }, (args) => {
-        return {
-          path: options[args.path],
-        }
-      });
-    }
+      build.onResolve({ filter: re, namespace: 'file' }, args => ({
+        path: options[args.path],
+      }));
+    },
   };
-}
+};
 
 function escapeRegExp(string) {
   // $& means the whole matched string
